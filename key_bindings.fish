29716f71f0582cf243f55f0e70c43b8f492ac698
Alin Panaitiu <alin.panaitiu@gmail.com>
2018-11-08 13:09:27 +0200

    Repaint after tree

--- key_bindings.fish
+++ key_bindings.fish
@@ -16,7 +16,7 @@ bind \e\e\[A history-token-search-backward
 bind \e\[1\;5B history-token-search-forward
 bind \e\e\[B history-token-search-forward
 bind \ce edit_command_buffer
-bind \et 'exa -l -T'
+bind \et 'exa -l -T; and commandline -f repaint'
 bind \eT '__tldr'
 bind \eH '__help_cmd'
 bind \eM '__mkdir_current'
