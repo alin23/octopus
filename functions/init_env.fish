08777d74bce603a298fdc527044ee055088674b7
Alin Panaitiu <alin.panaitiu@gmail.com>
2018-11-27 11:08:56 +0200

    Add all Github and Gitlap repos to cdpath

--- functions/init_env.fish
+++ functions/init_env.fish
@@ -2,7 +2,7 @@ function init_env
     set -xU PYTHONBREAKPOINT "pudb.set_trace"
     set -xU CLICOLOR "1"
     set -xU GOPATH "$HOME/.go"
-    set -xU GPG_TTY (tty)
+    set -xg GPG_TTY (tty)
     set -xU LANG "en_US.UTF-8"
     set -xU LANGUAGE "en_US.UTF-8"
     set -xU LC_ALL "en_US.UTF-8"
@@ -30,7 +30,7 @@ function init_env
         set -xU ANDROID_SDK_ROOT $HOME/Library/Android/sdk/
         set -xU SCRIPTS "$HOME/Documents/Scripts/Python" "$HOME/Documents/Scripts/Shell" "$HOME/Documents/Scripts/AppleScript"
         set -xg PATH "$HOME/.bin" "$HOME/.cargo/bin" $SCRIPTS "$ANDROID_SDK_ROOT/emulator" "/usr/local/opt/sqlite/bin" "/usr/local/opt/ncurses/bin" "/usr/local/bin" "/usr/local/sbin" $PATH "$HOME/.yarn-config/global/node_modules/.bin" "/usr/local/opt/go/libexec/bin" "$GOPATH/bin" "/usr/local/opt/android-sdk/bin" "$HOME/.local/bin" "/usr/local/opt/coreutils/libexec/gnubin" 2>/dev/null
-        set -xg CDPATH "$CDPATH" "$HOME" "$HOME/Projects" "$HOME/Github" "$HOME/Github/alin23" "$HOME/Gitlab" "$HOME/Gitlab/alin23" 2>/dev/null
+        set -xg CDPATH "$CDPATH" "$HOME" "$HOME/Projects" "$HOME/Github" "$HOME/Github/*" "$HOME/Gitlab" "$HOME/Gitlab/*" 2>/dev/null
         set -xU PGDATA /usr/local/var/postgres
         command_exists rustc; and set -xU RUST_SRC_PATH (rustc --print sysroot)"/lib/rustlib/src/rust/src"
     else
