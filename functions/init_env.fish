96880c7ba2fb810c389048c0ec8da61975bb094c
Alin Panaitiu <alin.panaitiu@gmail.com>
2018-11-28 16:31:04 +0200

    Make cdpath useful again

--- functions/init_env.fish
+++ functions/init_env.fish
@@ -30,7 +30,6 @@ function init_env
         set -xU ANDROID_SDK_ROOT $HOME/Library/Android/sdk/
         set -xU SCRIPTS "$HOME/Documents/Scripts/Python" "$HOME/Documents/Scripts/Shell" "$HOME/Documents/Scripts/AppleScript"
         set -xg PATH "$HOME/.bin" "$HOME/.cargo/bin" $SCRIPTS "$ANDROID_SDK_ROOT/emulator" "/usr/local/opt/sqlite/bin" "/usr/local/opt/ncurses/bin" "/usr/local/bin" "/usr/local/sbin" $PATH "$HOME/.yarn-config/global/node_modules/.bin" "/usr/local/opt/go/libexec/bin" "$GOPATH/bin" "/usr/local/opt/android-sdk/bin" "$HOME/.local/bin" "/usr/local/opt/coreutils/libexec/gnubin" 2>/dev/null
-        set -xg CDPATH "$CDPATH" "$HOME" "$HOME/Projects" "$HOME/Github" $HOME/Github/* "$HOME/Gitlab" $HOME/Gitlab/* 2>/dev/null
         set -xU PGDATA /usr/local/var/postgres
         command_exists rustc; and set -xU RUST_SRC_PATH (rustc --print sysroot)"/lib/rustlib/src/rust/src"
     else
@@ -45,14 +44,21 @@ function init_env
             set -xU EDITOR vim
         end
 
-        set -xg CDPATH "." "$CDPATH" "$HOME" "$HOME/Projects" "$HOME/Github" "$HOME/Github/alin23" "$HOME/Gitlab" "$HOME/Gitlab/alin23" 2>/dev/null
         set -xg PATH "$HOME/.bin" "$HOME/.cargo/bin" "/snap/bin" $PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" "$GOPATH/bin" "/usr/sbin" "/sbin" 2>/dev/null
         set -xg MANPATH "/home/linuxbrew/.linuxbrew/share/man" "$MANPATH"
         set -xg INFOPATH "/home/linuxbrew/.linuxbrew/share/info" "$INFOPATH"
     end
+    set -xg CDPATH "." "$CDPATH" "$HOME" "$HOME/Projects" "$HOME/.config" "$HOME/Github" (list_top_dirs /Users/alin/Github) "$HOME/Gitlab" (list_top_dirs /Users/alin/Gitlab) 2>/dev/null
+
     if test -d $HOME/n
         set -xU N_PREFIX $HOME/n
         set -xg PATH $N_PREFIX/bin $PATH
     end
     set -xU LPASS_AGENT_TIMEOUT 0
+end
+
+function list_top_dirs
+    if test -d $argv[1]
+        find -s $argv[1] \( -maxdepth 1 -and -type d \)
+    end
 end
