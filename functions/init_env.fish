bc1521ea9ee88d315c711207599fd121797e1f28
Alin Panaitiu <alin.panaitiu@gmail.com>
2018-12-16 22:32:43 +0100

    Fix find warning

--- functions/init_env.fish
+++ functions/init_env.fish
@@ -59,6 +59,6 @@ end
 
 function list_top_dirs
     if test -d $argv[1]
-        find $argv[1] \( -maxdepth 1 -and -type d \) | sort
+        find $argv[1] -maxdepth 1 -and -type d | sort
     end
 end
