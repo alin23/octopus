a0cb7e0bafc3fcd3c3824f62e28bbdd46e307a6b
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-03-08 22:00:01 +0200

    Update

--- functions/transfer.fish
+++ functions/transfer.fish
@@ -1,58 +1,11 @@
-function transfer -d "Upload to transfer.sh" -a file name
-
-    getopts $argv | while read -l 1 2
-        switch "$1"
-            case _
-                continue
-
-            case h help
-                echo "Usage: transfer [FILE] [NAME]"
-                echo
-                echo "Examples:"
-                echo "      transfer my-file.txt"
-                echo "      transfer my-file.txt my-file-new-name.txt"
-                echo "      echo my message text | transfer my-message.txt"
-                echo "      cat my-file.txt | transfer my-file-new-name.txt"
-                return
-
-            case \*
-                printf "transfer: '%s' is not a valid option\n" $1 >& 2
-                transfer --help >& 2
-                return 1
-        end
-    end
-
-    set -l tmp (mktemp -t transferXXX)
-
-    if test -z $name
-        if not isatty
-            set name $file
-        else if test -n "$file"
-            set name (basename $file)
-        end
-    end
-
-    if test -z $name
-        set name (random)
-    end
-
-    if not isatty
-        set file ""
-    end
-
-    set name (echo $name | sed -e 's/[^a-zA-Z0-9._-]/-/g')
-    set name (echo $name | sed -e 's/-\{1,\}/-/g')
-
-    if test -n "$file"
-        if not test -r "$file"
-            echo "transfer: can not read the file." > /dev/stderr
-            return 1
-        end
-        curl --progress-bar --upload-file $file https://transfer.sh/$name >> $tmp
+function transfer --description 'Upload a file to transfer.sh'
+    if [ $argv[1] ]
+        # write to output to tmpfile because of progress bar
+        set -l tmpfile ( mktemp -t transferXXX )
+        curl --progress-bar --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) >> $tmpfile
+        cat $tmpfile
+        command rm -f $tmpfile
     else
-        curl --progress-bar --upload-file - https://transfer.sh/$name >> $tmp
+        echo 'usage: transfer FILE_TO_TRANSFER'
     end
-
-    cat $tmp
-    rm -f $tmp
 end
