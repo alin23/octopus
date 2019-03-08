dfdb4fc277aa616adae5747f4a62df3309b7e4c4
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-03-08 22:30:01 +0200

    Update

--- functions/transfer.fish
+++ functions/transfer.fish
@@ -1,10 +1,35 @@
-function transfer --description 'Upload a file to transfer.sh'
-    if [ $argv[1] ]
-        set -l tmpfile ( mktemp -t transferXXX )
-        curl --progress-bar --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) >> $tmpfile
-        cat $tmpfile
-        command rm -f $tmpfile
-    else
-        echo 'usage: transfer FILE_TO_TRANSFER'
+function transfer -d "Upload to transfer.sh" -a file name
+    set -l tmp (mktemp -t transferXXX)
+
+    if test -z $name
+        if not isatty
+            set name $file
+        else if test -n "$file"
+            set name (basename $file)
+        end
     end
+
+    if test -z $name
+        set name (random)
+    end
+
+    if not isatty
+        set file ""
+    end
+
+    set name (echo $name | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+    set name (echo $name | sed -e 's/-\{1,\}/-/g')
+
+    if test -n "$file"
+        if not test -r "$file"
+            echo "transfer: can not read the file." > /dev/stderr
+            return 1
+        end
+        curl --progress-bar --upload-file $file https://transfer.sh/$name >> $tmp
+    else
+        curl --progress-bar --upload-file - https://transfer.sh/$name >> $tmp
+    end
+
+    cat $tmp
+    rm -f $tmp
 end
