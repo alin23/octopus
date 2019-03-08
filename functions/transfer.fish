f591ad8fae22b53c4c34217cbb544f6068329fc6
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-03-08 22:25:00 +0200

    Update

--- functions/transfer.fish
+++ functions/transfer.fish
@@ -1,6 +1,5 @@
 function transfer --description 'Upload a file to transfer.sh'
     if [ $argv[1] ]
-        # write to output to tmpfile because of progress bar
         set -l tmpfile ( mktemp -t transferXXX )
         curl --progress-bar --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) >> $tmpfile
         cat $tmpfile
