7cd0005529d0462644c34d5b064ed23ecc4bc9c0
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-05-27 13:00:01 +0300

    Update

--- functions/nowlogin.fish
+++
@@ -1,8 +0,0 @@
-function nowlogin
-    set team $argv[1]
-    if test -d "$HOME/Documents/Now/$team"
-        cp -f $HOME/Documents/Now/$team/* $HOME/.now/
-    else
-        echo "Team $team does not exist"
-    end
-end
