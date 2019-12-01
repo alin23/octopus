fb7ba3775257b1df08e4acaad06f64216eef693f
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-12-01 21:25:01 +0200

    Update

--- functions/transfer.fish
+++ functions/transfer.fish
@@ -26,6 +26,7 @@ function transfer -d "Upload to transfer.sh" -a file name maxDownloads maxDays
     set name (echo "$name" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
     set name (echo "$name" | sed -e 's/-\{1,\}/-/g')
 
+    echo "Uploading $file as $name for $maxDownloads downloads and $maxDays days"
     if test -n "$file"
         if not test -r "$file"
             echo "transfer: can not read the file." > /dev/stderr
@@ -34,6 +35,10 @@ function transfer -d "Upload to transfer.sh" -a file name maxDownloads maxDays
         curl -H "Max-Downloads: $maxDownloads" -H "Max-Days: $maxDays"  --progress-bar --upload-file $file https://transfer.sh/"$name" >> $tmp
     else
         curl -H "Max-Downloads: $maxDownloads" -H "Max-Days: $maxDays"  --progress-bar --upload-file - https://transfer.sh/"$name" >> $tmp
+    end
+
+    if command_exists pbcopy
+        tail -n 1 $tmp | pbcopy
     end
 
     cat $tmp
