bb7c099077dbb7f638861c5783305e32671f32a3
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-08 16:52:16 +0200

    update

--- functions/z.fish
+++ functions/z.fish
@@ -1,7 +1,7 @@
 function z
     if test (count $argv) -gt 0
         set _Z_RESULT (zoxide query $argv)
-        switch $_Z_RESULT
+        switch "$_Z_RESULT"
             case 'query: *'
                 cd (string sub -s 8 -- "$_Z_RESULT")
                 commandline -f repaint
