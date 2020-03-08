900817489a6554123f2537d812631e75f4fa48e3
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-08 15:45:00 +0200

    Update

--- functions/varcache.fish
+++ functions/varcache.fish
@@ -1,13 +1,25 @@
 function varcache
-    if test (count $argv) -gt 0
-        set _Z_RESULT (zoxide query $argv)
-        switch $_Z_RESULT
-            case 'query: *'
-                cd (string sub -s 8 -- "$_Z_RESULT")
-                commandline -f repaint
-            case '*'
-                echo "$_Z_RESULT"
+    if test (count $argv) -lt 3
+        echo "Usage: varcache KEY CONTENT EXPIRATION"
+        return 1
+    end
+    set varkey '__VARCACHE_'(string escape --style var -- $argv[1])
+    set timevarkey $varkey'_CACHED_AT'
+    set content $argv[2]
+    set expiration $argv[3]
+
+    if set -q $timevarkey
+        set cached_at $$timevarkey
+        set exp (date -v +$expiration +%s)
+        if test $exp -gt $cached_at
+            echo $$varkey
+            return 0
         end
+    else
+        set -U $varkey $content
+        set -U $timevarkey (date +%s)
+        echo $content
+        return 2
     end
 end
 
