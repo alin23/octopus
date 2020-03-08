dc16e6cf547684095287a16cc15373244ee0b1b3
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-08 20:15:00 +0200

    Update

---
+++ functions/varcache-expire.fish
@@ -0,0 +1,12 @@
+function varcache-expire
+    if test (count $argv) -lt 3
+        echo "Usage: varcache KEY"
+        return 1
+    end
+
+    set varkey '__VARCACHE_'(string escape --style var -- $argv[1])
+    set timevarkey $varkey'_CACHED_AT'
+
+    set -e $varkey
+    set -e $timevarkey
+end
