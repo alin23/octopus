8c1f6d188830945721bde1f10ecb517d50b56e3f
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-08 16:05:00 +0200

    Update

--- functions/varcache.fish
+++ functions/varcache.fish
@@ -1,11 +1,11 @@
 function varcache
     if test (count $argv) -lt 3
-        echo "Usage: varcache KEY CONTENT EXPIRATION [compressed]"
+        echo "Usage: varcache KEY CMD EXPIRATION [compressed]"
         return 1
     end
     set varkey '__VARCACHE_'(string escape --style var -- $argv[1])
     set timevarkey $varkey'_CACHED_AT'
-    set content $argv[2]
+    set cmd $argv[2]
     set expiration $argv[3]
 
     if set -q $timevarkey
@@ -20,6 +20,11 @@ function varcache
             return 0
         end
     else
+        if test (count $argv) -ge 4; and test "$argv[4]" = "compressed"
+            set cmd $cmd' | gzip | base64'
+        end
+
+        set content (eval $cmd)
         set -U $varkey $content
         set -U $timevarkey (date +%s)
         if test (count $argv) -ge 4; and test "$argv[4]" = "compressed"
