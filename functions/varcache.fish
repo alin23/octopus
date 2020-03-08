2573583f16cfc38c12cb6355e4e3baf0bff7dfd2
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-08 15:55:00 +0200

    Update

--- functions/varcache.fish
+++ functions/varcache.fish
@@ -1,6 +1,6 @@
 function varcache
     if test (count $argv) -lt 3
-        echo "Usage: varcache KEY CONTENT EXPIRATION"
+        echo "Usage: varcache KEY CONTENT EXPIRATION [compressed]"
         return 1
     end
     set varkey '__VARCACHE_'(string escape --style var -- $argv[1])
@@ -12,7 +12,7 @@ function varcache
         set cached_at $$timevarkey
         set exp (date -v +$expiration +%s)
         if test $exp -gt $cached_at
-            if test (count $argv) -ge 4; and test "$argv[4]" = "gzip"
+            if test (count $argv) -ge 4; and test "$argv[4]" = "compressed"
                 echo $$varkey | base64 -D | gunzip
             else
                 echo $$varkey
@@ -20,13 +20,13 @@ function varcache
             return 0
         end
     else
-        if test (count $argv) -ge 4; and test "$argv[4]" = "gzip"
-            set -U $varkey (echo $content | gzip | base64)
-        else
-            set -U $varkey $content
-        end
+        set -U $varkey $content
         set -U $timevarkey (date +%s)
-        echo $content
+        if test (count $argv) -ge 4; and test "$argv[4]" = "compressed"
+            echo $content | base64 -D | gunzip
+        else
+            echo $content
+        end
         return 2
     end
 end
