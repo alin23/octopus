43884863c421da56294819ed72cbf9836c635ff6
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-09 20:50:00 +0200

    Update

--- functions/addbin.fish
+++ functions/addbin.fish
@@ -1,5 +1,11 @@
-function pipeto
-    echo ''
-    eval (commandline)' | '$argv[1]
-    commandline -f repaint
+function addbin
+    argparse -x url,file 'u/url=' 'f/file=' 'n/name=?' -- $argv
+    or return 1
+
+    if set -q _flag_url
+        wget $_flag_url -O $HOME/.bin/shared/$_flag_name
+    else
+        cp $_flag_file $HOME/.bin/shared/$_flag_name
+    end
+    chmod +x $HOME/.bin/shared/*
 end
