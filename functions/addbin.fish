d0bc6028f961465cada3bc54fd1fcaba1aaddc51
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-09 20:55:00 +0200

    Update

--- functions/addbin.fish
+++ functions/addbin.fish
@@ -2,10 +2,16 @@ function addbin
     argparse -x url,file 'u/url=' 'f/file=' 'n/name=?' -- $argv
     or return 1
 
-    if set -q _flag_url
-        wget $_flag_url -O $HOME/.bin/shared/$_flag_name
+    if set -q _flag_name
+        set binary_name $_flag_name
     else
-        cp $_flag_file $HOME/.bin/shared/$_flag_name
+        set binary_name (basename "$_flag_url$_flag_file")
+    end
+
+    if set -q _flag_url
+        wget -O "$HOME/.bin/shared/$_flag_name" "$_flag_url"
+    else
+        cp $_flag_file "$HOME/.bin/shared/$_flag_name"
     end
     chmod +x $HOME/.bin/shared/*
 end
