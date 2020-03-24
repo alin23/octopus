76173454fb285f558886834f2d2357e2dcd91407
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-24 13:15:01 +0200

    Update

--- functions/upload.fish
+++ functions/upload.fish
@@ -1,6 +1,11 @@
-function pyenv-run
-    if pyenv shell $argv[1]
-        eval $argv[2..-1]
-        pyenv shell -
+function upload
+    argparse --name upload 'u/url=' 'n/name=' 'd/dir' -- $argv[2..-1]
+
+    set file_to_upload $argv[1]
+    set dir_to_upload alinpanaitiu
+    if set -q _flag_dir
+        set dir_to_upload $_flag_dir
     end
+
+    rsync -v -e ssh $file_to_upload noiseblend:/static/$dir_to_upload/$_flag_name
 end
