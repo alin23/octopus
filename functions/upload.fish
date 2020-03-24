99cb35d018203bbe1e8b99a1fbb3edb0261c1e58
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-24 13:20:01 +0200

    Update

--- functions/upload.fish
+++ functions/upload.fish
@@ -1,10 +1,15 @@
 function upload
     argparse --name upload 'u/url=' 'n/name=' 'd/dir' -- $argv[2..-1]
 
-    set file_to_upload $argv[1]
-    set dir_to_upload alinpanaitiu
+    set -l file_to_upload $argv[1]
+    set -l dir_to_upload alinpanaitiu
     if set -q _flag_dir
         set dir_to_upload $_flag_dir
+    end
+
+    if test -d $file_to_upload
+        zip -9 -r /tmp/(basename $file_to_upload).zip $file_to_upload
+        set file_to_upload /tmp/(basename $file_to_upload).zip
     end
 
     rsync -v -e ssh $file_to_upload noiseblend:/static/$dir_to_upload/$_flag_name
