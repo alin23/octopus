5052e88a3e9dfd28959044cf5d8302408d5e0404
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-05-10 16:05:00 +0300

    Update

--- functions/batview.fish
+++ functions/batview.fish
@@ -1,4 +1,16 @@
 function batview
-    export LESS='-r -R --ignore-case --status-column --LONG-PROMPT --HILITE-UNREAD --tabs=4 --no-init --window=-4'
-    bat --paging=always --pager=less $argv
+    set -xg LESS '-r -R --ignore-case --status-column --LONG-PROMPT --HILITE-UNREAD --tabs=4 --no-init --window=-4'
+    if string match -q -- "*binary" (file --mime $argv[1])
+        if test -n "$HEXYL_EXISTS"
+            hexyl --color always $argv[1]
+        else
+            xxd $argv[1] | less
+        end
+    else
+        if test -n "$BAT_EXISTS"
+            bat --paging=always --pager=less $argv[1]
+        else
+            mcview $argv[1]
+        end
+    end
 end
