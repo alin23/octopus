f5f7eeb63a190c82be6d9c50dd9a663b4d7b6e1e
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-06-06 18:05:00 +0300

    Update

--- functions/with-proxy.fish
+++ functions/with-proxy.fish
@@ -1,4 +1,10 @@
-function fzf-jq -d "Jq with fzf live preview"
-    set json (string escape -n -- $argv)
-    echo '' | fzf --print-query --preview "cat $json | jq {q}"
+function with-proxy -d "Run command with proxy"
+    set -xg https_proxy (shuf -n 1 ~/Documents/http_proxies.txt)
+    set -xg http_proxy $https_proxy
+
+    while not $argv
+        echo Retrying request
+        set -xg https_proxy (shuf -n 1 ~/Documents/http_proxies.txt)
+        set -xg http_proxy $https_proxy
+    end
 end
