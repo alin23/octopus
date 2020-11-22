82940d58b3fee67dfadae815aa20d56bfabfe2e2
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-11-22 14:59:15 +0200

    Update

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -1,15 +1,15 @@
-function rpbcopy
+function pbcopy
     set unamestr (uname)
     if test "$unamestr" = 'Linux'
         echo -e "Linux SSH: Copying to Clipboard."
         cat | nc -q0 localhost 2224
-    else if test "$unamestr" = 'FreeBSD'
+    else if string match -qr 'FreeBSD|Darwin' -- "$unamestr"
         if test -n "$SSH_CLIENT"
           echo -e "OS X SSH: Copying to Clipboard."
           cat | nc -c localhost 2224
         else
           echo -e "OS X Local: Copying to Clipboard."
-          cat | pbcopy
+          cat | command pbcopy
         end
     end
 end
