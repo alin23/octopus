40b8db68c9949f4529f3b4538d4d37dc3586248a
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-11-23 18:59:59 +0200

    Update

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -1,16 +1,22 @@
+function tempfile
+    if command_exists tempfile
+        command tempfile
+    else
+        command mktemp
+    end
+end
+
 function pbcopy
     set unamestr (uname)
-    set -xg TMPF_PBCOPY (tempfile)
     if test "$unamestr" = 'Linux'
-        echo -e "Linux SSH: Copying to Clipboard."
+        set -xg TMPF_PBCOPY (tempfile)
         cat | tee $TMPF_PBCOPY | nc -q0 localhost 2224
     else if string match -qr 'FreeBSD|Darwin' -- "$unamestr"
         if test -n "$SSH_CLIENT"
-          echo -e "OS X SSH: Copying to Clipboard."
-          cat | nc -c localhost 2224
+            set -xg TMPF_PBCOPY (tempfile)
+            cat | tee $TMPF_PBCOPY | nc -c localhost 2224
         else
-          echo -e "OS X Local: Copying to Clipboard."
-          cat | command pbcopy
+            cat | command pbcopy
         end
     end
 end
--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -1,7 +1,7 @@
 function pbpaste
     if set -q TMPF_PBCOPY
         cat $TMPF_PBCOPY
-    else
+    else if command_exists pbpaste
         command pbpaste
     end
 end
