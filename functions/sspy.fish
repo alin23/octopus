027c57ef7e76ad6a9a13204ad59ce52a78e1c84e
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-12-07 15:56:28 +0200

    Update

--- functions/sspy.fish
+++ functions/sspy.fish
@@ -1,3 +1,16 @@
 function sspy
-    subl (string replace -r '\.pyc$' .py (pywhich $argv[1]))
+    argparse --name sspy 'd/dir' 'r/remote' -- $argv
+    set module (string replace -r '\.pyc$' .py (pywhich $argv[1]))
+
+    if set -q _flag_remote
+        set subl (which rsub)
+    else
+        set subl (which subl)
+    end
+
+    if set -q _flag_dir
+        $subl (dirname $module)
+    else
+        $subl $module
+    end
 end
