c041fb244a71eb41baeeeeec4701426bd3b2a115
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-12-17 12:34:31 +0200

    Update

--- conf.d/init.fish
+++ conf.d/init.fish
@@ -1,3 +1,11 @@
+init_env 2>/dev/null
+
+if not set -q NO_FISH_INIT; and not set -q LC_NO_FISH_INIT
+    init_abbr 2>/dev/null
+    init_pyenv 2>/dev/null
+    init_rbenv 2>/dev/null
+end
+
 if command_exists gdate
     set -xg _date_command 'gdate'
 else
@@ -8,12 +16,4 @@ if command_exists gbase64
     set -xg _base64_command 'gbase64'
 else
     set -xg _base64_command 'base64'
-end
-
-init_env 2>/dev/null
-
-if not set -q NO_FISH_INIT; and not set -q LC_NO_FISH_INIT
-    init_abbr 2>/dev/null
-    init_pyenv 2>/dev/null
-    init_rbenv 2>/dev/null
 end
