08635e3a93683ddd849436fe8c2fc78c1e859896
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-01-26 18:43:51 +0200

    Update

--- functions/ap.fish
+++ functions/ap.fish
@@ -1,3 +1,12 @@
 function ap -d "Add to path"
-    set -xU fish_user_paths $argv $fish_user_paths
+    argparse --name ap 'g/global' 'a/all' -- $argv
+
+    if set -q _flag_all
+        set -xg PATH $argv $PATH
+        set -xU fish_user_paths $argv $fish_user_paths
+    else if set -q _flag_global
+        set -xg PATH $argv $PATH
+    else
+        set -xU fish_user_paths $argv $fish_user_paths
+    end
 end
--- functions/rp.fish
+++ functions/rp.fish
@@ -1,3 +1,12 @@
 function rp -d "Remove from path"
-    set -xU fish_user_paths (string replace -a $argv "" -- $fish_user_paths)
+    argparse --name rp 'g/global' 'a/all' -- $argv
+
+    if set -q _flag_all
+        set -xg PATH (string replace -a $argv "" -- $PATH)
+        set -xU fish_user_paths (string replace -a $argv "" -- $fish_user_paths)
+    else if set -q _flag_global
+        set -xg PATH (string replace -a $argv "" -- $PATH)
+    else
+        set -xU fish_user_paths (string replace -a $argv "" -- $fish_user_paths)
+    end
 end
