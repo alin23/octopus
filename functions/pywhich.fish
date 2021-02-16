eb861350dd4f4a98546436a38138d03a143ea5dc
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-02-16 13:10:04 +0200

    Update

--- functions/pywhich.fish
+++ functions/pywhich.fish
@@ -1,3 +1,11 @@
 function pywhich
-    python -c "import $argv[1]; print($argv[1].__file__)"
+    argparse --name sspy 'd/dir' -- $argv
+
+    set module (python -c "import $argv[1]; print($argv[1].__file__)")
+
+    if set -q _flag_dir
+        echo (dirname $module)
+    else
+        echo $module
+    end
 end
--- functions/sspy.fish
+++ functions/sspy.fish
@@ -1,11 +1,15 @@
 function sspy
-    argparse --name sspy 'd/dir' 'r/remote' -- $argv
+    argparse --name sspy 'd/dir' 'a/add' 'r/remote' -- $argv
     set module (string replace -r '\.pyc$' .py (pywhich $argv[1]))
 
     if set -q _flag_remote
         set subl (which rsub)
     else
         set subl (which subl)
+    end
+
+    if set -q _flag_add
+        set subl $subl --add
     end
 
     if set -q _flag_dir
