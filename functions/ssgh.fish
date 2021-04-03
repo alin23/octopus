ca55e404de4a9579b17c6b97a76234f1ee322075
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-04-03 18:53:01 +0300

    Update

--- functions/ssgh.fish
+++ functions/ssgh.fish
@@ -1,4 +1,4 @@
 function ssgh
-    git clone --recursive git@github.com:$argv[1] $HOME/Github/$argv[1]; or true
+    git clone git@github.com:$argv[1] $HOME/Github/$argv[1]; or true
     subl $HOME/Github/$argv[1]
 end
--- functions/ssgl.fish
+++ functions/ssgl.fish
@@ -1,4 +1,4 @@
-function ssgh
-    git clone --recursive git@github.com:$argv[1] $HOME/Gitlab/$argv[1]; or true
+function ssgl
+    git clone git@gitlab.com:$argv[1] $HOME/Gitlab/$argv[1]; or true
     subl $HOME/Gitlab/$argv[1]
 end
