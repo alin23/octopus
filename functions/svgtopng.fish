bd0f936468214fa9163a8f0a496fe8b29665e258
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-05-10 15:23:45 +0300

    svgtopng

--- functions/svgtopng.fish
+++ functions/svgtopng.fish
@@ -1,9 +1,10 @@
-function svgtopng -a svg png
-    if empty "$png"
-        set png (basename -s .svg $svg).png
-    end
+function svgtopng
+    argparse --name svgtopng 'd/dpi=' -- $argv
+    set dpi (set -q _flag_dpi; and echo $_flag_dpi; or echo 200)
+    set svg $argv[1]
+    set png (set -q argv[2]; and echo $argv[2]; or echo (basename -s .svg $svg).png)
 
-    inkscape \
-        --export-filename="$png" --export-dpi=200 \
+    echo inkscape \
+        --export-filename="$png" --export-dpi=$dpi \
         --export-background-opacity=0 "$svg"
 end
