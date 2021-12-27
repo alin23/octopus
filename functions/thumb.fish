4778b0d64a888ddb3fde43dd10922bec590438b9
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-12-27 13:01:56 +0200

    Fix thumb

--- functions/thumb.fish
+++ functions/thumb.fish
@@ -1,16 +1,16 @@
 function thumb
     set -l ext (string match -r '\..+$' $argv[1])
-    set format "%s_$argv[2]"
-    if test -n $argv[3]
+    set format "%s_"(basename -s $ext "$argv[1]")
+    if set -q argv[3]
         set format $argv[3]
     end
     vipsthumbnail $argv[1] \
-    --vips-progress \
-    --linear \
-    --size=$argv[2] \
-    --vips-concurrency=4 \
-    -o "$format$ext" \
-    --eprofile='/System/Library/ColorSync/Profiles/sRGB Profile.icc' \
-    --delete \
-    --rotate
+        --vips-progress \
+        --linear \
+        --size=$argv[2] \
+        --vips-concurrency=4 \
+        -o "$format$ext" \
+        --eprofile='/System/Library/ColorSync/Profiles/sRGB Profile.icc' \
+        --delete \
+        --rotate
 end
