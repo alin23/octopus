38f13366ca0aac79df85c3e0eed62fef767c6e4b
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-02-05 14:10:31 +0200

    Fix replace

--- functions/duplicate_last_token.fish
+++ functions/duplicate_last_token.fish
@@ -1,10 +1,10 @@
 function duplicate_last_token
     set -l cur (commandline --cursor)
     set -l until_cursor (commandline -c)
-    set -l trimmed (string trim -r $until_cursor)
-    set -l spaces_count (string length (string match -r '\s*$' $until_cursor))
+    set -l trimmed (string trim -r -- $until_cursor)
+    set -l spaces_count (string length -- (string match -r '\s*$' -- $until_cursor))
     if test $spaces_count -gt 0
-        commandline -r (string replace $until_cursor $trimmed (commandline))
+        commandline -r (string replace -- $until_cursor $trimmed (commandline))
         set cur (math $cur - $spaces_count)
     end
 
--- functions/file-decrypt.fish
+++ functions/file-decrypt.fish
@@ -1,5 +1,5 @@
 function file-decrypt
-    set -l DECRYPTED_FILENAME (string replace -r '\.gpg$' '' $argv[1])
+    set -l DECRYPTED_FILENAME (string replace -r '\.gpg$' '' -- $argv[1])
     if test $status -eq 1
         set DECRYPTED_FILENAME "$argv[1].decrypted"
     end
