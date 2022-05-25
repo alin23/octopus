74752c45b9dad8444cff41d4d6a760d31e4ed324
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-05-25 11:32:11 +0300

    Thumb update

--- functions/thumb.fish
+++ functions/thumb.fish
@@ -1,16 +1,27 @@
-function thumb -a filename size output
-    set -l ext (string match -r '\..+$' $filename)
-    set format "%s_$size"
-    if set -q argv[3]
-        set format $output
+function thumb
+    argparse --name thumb 's/size=' 'o/outputdir=' c/crop -- $argv
+
+    for filename in $argv
+        set -l ext (string match -r '\..+$' $filename)
+        set format "%s_$size"
+        if set -q _flag_outputdir
+            set format $_flag_outputdir
+        end
+
+        set restargs
+        if set -q _flag_crop
+            set restargs $restargs --smartcrop attention
+        end
+
+        echo Creating thumbnail for (set_color -o yellow)$filename(set_color normal) at (set_color -o green)$format$ext(set_color normal)
+
+        vipsthumbnail $filename \
+            --vips-progress \
+            --linear \
+            --size=$size \
+            --vips-concurrency=(nproc) \
+            -o "$format$ext" \
+            --delete \
+            --rotate $restargs
     end
-    vipsthumbnail $filename \
-        --vips-progress \
-        --linear \
-        --size=$size \
-        --vips-concurrency=4 \
-        -o "$format$ext" \
-        --eprofile='/System/Library/ColorSync/Profiles/sRGB Profile.icc' \
-        --delete \
-        --rotate
 end
